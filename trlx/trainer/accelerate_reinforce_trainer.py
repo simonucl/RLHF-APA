1dbba6107f51f209ad02451c8fb3d857da4849c8
Kanishk Gandhi <kanishk050296@gmail.com>
2024-03-13 23:24:46 -0700

    debug

--- trlx/trainer/accelerate_reinforce_trainer.py
+++ trlx/trainer/accelerate_reinforce_trainer.py
@@ -179,7 +179,7 @@ class AccelerateReinforceTrainer(AccelerateRLTrainerNoV):
             tokens = torch.cat((query_tensors, response_tensors), dim=1)
             attention_mask = tokens.not_equal(self.tokenizer.pad_token_id).long().to(tokens.device)
             print(tokens.shape, attention_mask.shape)
-            outputs = self.model(tokens, attention_mask, return_dict=True)
+            outputs = self.model(input_ids=tokens, attention_mask=attention_mask, return_dict=True)
             logits = outputs.logits
             logprobs = logprobs_of_labels(logits[:, :-1, :], tokens[:, 1:])
 
