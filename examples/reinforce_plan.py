d52d8d0ae0b78bd3ec9a762afc65222f562c7ff2
Kanishk Gandhi <kanishk050296@gmail.com>
2024-03-14 15:25:13 -0700

    change num rollouts

--- examples/reinforce_plan.py
+++ examples/reinforce_plan.py
@@ -101,7 +101,7 @@ def main(hparams={}):
     scheduler=SchedulerConfig(name="cosine_annealing", kwargs=dict(T_max=10000, eta_min=1e-6)),
     method=ReinforceConfig(
         name="ReinforceConfig",
-        num_rollouts=1024,
+        num_rollouts=128,
         chunk_size=64,
         ppo_epochs=2,
         init_kl_coef=0.1,
