51d46d0697ed89103f67d9f6c53293c3c4602f1f
Kanishk Gandhi <kanishk050296@gmail.com>
2024-03-14 18:34:05 -0700

    debug

--- examples/reinforce_plan.py
+++ examples/reinforce_plan.py
@@ -97,7 +97,7 @@ def main(hparams={}):
     ),
     model=ModelConfig(model_path='/scr/kanishkg/rational-cot/models/sft-mix-4-cd5e5/checkpoint-45500/', num_layers_unfrozen=-1),
     tokenizer=TokenizerConfig(tokenizer_path="EleutherAI/gpt-neo-1.3B", padding_side="left"),
-    optimizer=OptimizerConfig(name="adamw", kwargs=dict(lr=1e-6, betas=(0.9, 0.95), eps=1.0e-8, weight_decay=1.0e-6)),
+    optimizer=OptimizerConfig(name="adamw", kwargs=dict(lr=1e-5, betas=(0.9, 0.95), eps=1.0e-8, weight_decay=1.0e-6)),
     scheduler=SchedulerConfig(name="cosine_annealing", kwargs=dict(T_max=10000, eta_min=1e-6)),
     method=ReinforceConfig(
         name="ReinforceConfig",
