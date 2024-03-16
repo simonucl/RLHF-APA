22c75a3e170f60fa0528e4c3618213528e441e55
Kanishk Gandhi <kanishk050296@gmail.com>
2024-03-15 17:09:16 -0700

    debug save/load

--- trlx/trainer/accelerate_sppo_trainer.py
+++ trlx/trainer/accelerate_sppo_trainer.py
@@ -71,7 +71,7 @@ class AccelerateSPPOTrainer(AccelerateRLTrainer):
         )
         # import logging
         # logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-        # self.accelerator.load_state('/scr/kanishkg/trl/outputs/checkpoint_02800/')
+        self.accelerator.load_state('/scr/kanishkg/trl/outputs2/checkpoint_00001/')
         self.store.clear_history()  # Clear the rollout store
 
         # Setup a reference model when hydra heads are not used
