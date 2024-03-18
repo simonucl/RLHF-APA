df7257c214256e1a0792a82ee0ddd04988c3dd31
Kanishk Gandhi <kanishk050296@gmail.com>
2024-03-18 10:41:32 -0700

    debg

--- examples/apa_plan.py
+++ examples/apa_plan.py
@@ -30,12 +30,12 @@ import random
 # parser = argparse.ArgumentParser(description='Train with APA.')
 # parser.add_argument('--ckpt', type=str, default=None, help='Path to the checkpoint to load')
 
-RANDOM_SEED = 44
+RANDOM_SEED = 45
 LOSS = "square" # "square" or "log", square for APA and log for AWR
 ADV_COEFF_SQ = 0.5 # TODO: tune this. Options: 0.5, 1, 5, 10
 LR = 1e-6 # TODO: tune this. Options: 1e-5, 1e-7
 ADV_COEFF_LOG = 0.5
-OUTPUT_DIR = "/scr/kanishkg/trl/outputs_5"
+OUTPUT_DIR = "/scr/kanishkg/trl/outputs_6"
 
 random.seed(RANDOM_SEED)
 np.random.seed(RANDOM_SEED)
@@ -60,7 +60,7 @@ def main(hparams={}):
         eval_interval=100,
         pipeline="PromptPipeline",
         trainer="AccelerateSPPOTrainer",
-        checkpoint_dir="/scr/kanishkg/trl/checkpoints/apa_plan5",
+        checkpoint_dir="/scr/kanishkg/trl/checkpoints/apa_plan6",
         seed=RANDOM_SEED,
     ),
     model=ModelConfig(model_path='/scr/kanishkg/rational-cot/models/sft-mix-4-cd5e5/checkpoint-45500/', num_layers_unfrozen=-1),
